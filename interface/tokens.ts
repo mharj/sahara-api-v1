dfa5c2226697309172457d4729160726c7919e51
Marko Harjula <marko.harjula@gmail.com>
2019-05-23 07:13:48 +0300

    accesstoken interface

---
+++ interface/tokens.ts
@@ -0,0 +1,10 @@
+export interface IAccessTokenPayload {
+	email: string;
+	name: string;
+	picture?: string;
+	token_type: string;
+	nonce: string;
+	roles: string[];
+	scope: string;
+	sid: string;
+}
