148cf5bee6db22c2aa4d50cafb07975ebb2a94cd
Marko Harjula <marko.harjula@gmail.com>
2019-08-30 10:14:03 +0300

    added IdToken interfaces

--- interface/tokens.ts
+++ interface/tokens.ts
@@ -20,3 +20,21 @@ export interface IAccessTokenResponse {
 export interface IAccessTokenExchangeResponse extends IAccessTokenResponse {
 	issued_token_type: string;
 }
+
+export interface IIdTokenPayload {
+	email?: string;
+	name?: string;
+	picture?: string;
+	nonce: string;
+}
+
+export interface IIdTokenResponse {
+	id_token: string;
+	scope: string;
+	refresh_token?: string;
+	token_type: string;
+	expires_in: number;
+}
+export interface IIdTokenExchangeResponse extends IIdTokenResponse {
+	issued_token_type: string;
+}
