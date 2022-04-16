5e84224ed9acdb7e262df47e21720219088cac08
Marko Harjula <marko.harjula@avanio.com>
2022-04-16 11:57:42 +0300

    fix api limit

--- userUtil.ts
+++ userUtil.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-useless-escape */
-export const isValidUsername = /^[a-z0-9]{4,}$/;
+export const isValidUsername = /^[a-z0-9]{3,}$/;
 
 export const isValidEmail = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
