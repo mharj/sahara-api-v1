90b2da49d168d5f0ca3bffc196ff976a567f017a
Marko Harjula <marko.harjula@avanio.com>
2022-04-16 17:03:35 +0300

    added server packet

--- websocket/weather/server.ts
+++ websocket/weather/server.ts
@@ -1,12 +1,19 @@
+export interface WeatherData {
+	id: number;
+	icon: string;
+	name: string;
+	temp: number;
+	wid: number;
+	dt: number;
+}
+
 interface Weather {
 	stype: 'weather';
-	data: {
-		id: number;
-		icon: string;
-		name: string;
-		temp: number;
-		wid: number;
-	};
+	data: WeatherData;
+}
+
+export function isWeatherServerPacket(data: unknown): data is Weather {
+	return typeof data === 'object' && data !== null && 'stype' in data;
 }
 
 export type WeatherServerPacket = Weather;
